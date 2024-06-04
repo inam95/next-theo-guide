4fa28b155453a73c6ed1579a10f411d9a87cdaa1
inam95 <mfihassan99@gmail.com>
2024-06-04 19:47:38 +0530

    add uploadthing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TopNav } from "./_components/top-nav";
+import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 export const metadata = {
   title: "Gallery",
@@ -17,6 +21,15 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
+      <NextSSRPlugin
+        /**
+         * The `extractRouterConfig` will extract **only** the route configs
+         * from the router to prevent additional information from being
+         * leaked to the client. The data passed to the client is the same
+         * as if you were to fetch `/api/uploadthing` directly.
+         */
+        routerConfig={extractRouterConfig(ourFileRouter)}
+      />
       <html lang="en">
         <body className={`${inter.className} flex flex-col gap-4`}>
           <TopNav />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,11 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/4b90284a-9498-4fe7-b88f-1d24e8c7cdbd-1d.jpg",
-  "https://utfs.io/f/d0579066-9548-407c-a42f-1f727daf6e15-1e.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 async function Images() {
   const images = await db.query.images.findMany({ orderBy: (model, { asc }) => asc(model.id) });
   return (
     <div className="flex flex-wrap gap-4">
-      {[...images, ...images].map((image, index) => (
+      {[...images].map((image, index) => (
         <div key={index} className="w-48">
           <img src={image.url} alt="" />
           <div>{image.name}</div>
