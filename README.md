caf2b85495fc912828fb374f91fdcaa9a3676557
inam95 <mfihassan99@gmail.com>
2024-06-04 18:35:17 +0530

    use auth for home page

--- README.md
+++ README.md
@@ -7,8 +7,9 @@
 - [x] Tidy up build process
 - [x] Actually setup a database (vercel postgres)
 - [x] Attach db to UI
-- [ ] Add authentication (w/ clerk)
+- [x] Add authentication (w/ clerk)
 - [ ] Add image upload
+- [ ] "taint" (server-only)
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
@@ -12,19 +13,29 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({ orderBy: (model, { asc }) => asc(model.id) });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images].map((image, index) => (
+        <div key={index} className="w-48">
+          <img src={image.url} alt="" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images].map((image, index) => (
-          <div key={index} className="w-48">
-            <img src={image.url} alt="" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
